ac65e89feef6de67e1162b2aed43077603143c62
Santosh Dhanyamraju <geeniuss01@gmail.com>
2013-02-19 11:52:17 +0530

    watershed - changed logic; incomplete

--- practice/watershed/watershed.py
+++ practice/watershed/watershed.py
@@ -6,74 +6,80 @@ Created on Feb 16, 2013
 import sys
 import datetime
 import logging
+from platform import node
+import itertools
+import random
+
+class Node:
+    cnt=0
+    def __init__(self, data, nextNode=None):
+        self.__data = data
+        self.__nextNode = nextNode
+        self.__label=None
+
+    def get_data(self):
+        return self.__data
 
 
+    def get_next_node(self):
+        return self.__nextNode
+
+
+    def set_data(self, value):
+        self.__data = value
+
+
+    def set_next_node(self, value):
+        self.__nextNode = value
+
+    def getLabel(self):
+        '''recursively calculates and returns labels'''
+        #logging.info(self)
+        if self.__label==None:
+            if self.__nextNode==None or self.__nextNode==self:
+                self.__label=str(Node.cnt)
+                Node.cnt+=1
+            else:
+                self.__label=self.__nextNode.getLabel()
+        return self.__label
+    
+    def __str__(self):
+        return str(self.__data)
+    
+    def __repr__(self):
+        return str(self.getLabel())
+    
 def getSmallestNeighbour(arry, x, y):
     ''' returns index of smallest value surrounding the value at position
     in case of tie N(North),W,E,S order is used'''
-    ar = []
-    logging.info('x,y '+str(x)+','+str(y))
+    ar = [] 
+    #logging.info('x,y '+str(x)+','+str(y))
     if y > 0:
-        ar.append((arry[x][y - 1]['data'], x, y - 1, 0))  # North
+        ar.append((arry[x][y - 1].get_data(), x, y - 1, 0))  # West
     if x > 0:
-        ar.append((arry[x - 1][y]['data'], x - 1, y, 1))  # West  
-    if x < len(arry[0]) - 1:
-        ar.append((arry[x + 1][y]['data'], x + 1, y, 2))  # East
-    if y < len(arry) - 1:
-        ar.append((arry[x][y + 1]['data'], x, y + 1, 3))  # South
+        ar.append((arry[x - 1][y].get_data(), x - 1, y, 1))  # North  
+    if x < len(arry) - 1:
+        ar.append((arry[x + 1][y].get_data(), x + 1, y, 2))  # South
+    if y < len(arry[0]) - 1:
+        ar.append((arry[x][y + 1].get_data(), x, y + 1, 3))  # East
     ar.sort(key=lambda d:d[0])
     #logging.info('ar '+str(ar))
     # see if it is sink: current element is smaller than all its surrounding elements
-    if ar[0][0] >= arry[x][y]:
-        arry[x][y]['nxt']=None
-    if ar[0][0] != ar[1][0]:
-        arry[x][y]['nxt']=(ar[0][1],ar[0][2])
+    if ar[0][0] >= arry[x][y].get_data():
+        arry[x][y].set_next_node(arry[x][y])
+        logging.info(str(arry[x][y])+'sync')
+    elif ar[0][0] != ar[1][0]:
+        arry[x][y].set_next_node(arry[ar[0][1]][ar[0][2]])
+        logging.info('%d %d --> %d %d'%(x,y,ar[0][1],ar[0][2]))
     else :
         # tie : we return as per the direction of the item
-        if ar[0][3] < ar[1][3]: arry[x][y]['nxt']=(ar[0][1],ar[0][2])    
-        else : arry[x][y]['nxt']=(ar[1][1],ar[1][2])
-    
-    
-def connectNode(connected_map, smallest_neighbour, current_node):
-    ''' connected_map is an array of sets
-    makes smallest_neighbiur and current_node to belong to same set
-    merges two sets if both nodes are already present
-    creates new set if necessary
-    '''
-    
-    logging.info('map '+str(connected_map))
-    index_smallest_neighbour=(-1,-1)
-    index_current_node=(-1,-1)
-    for i,v in enumerate(connected_map):
-        for j,v1 in enumerate(v):
-            if v1[1]==current_node[1] and v1[2]==current_node[2]: index_current_node=(i,j)
-            if v1[1]==smallest_neighbour[1] and v1[2]==smallest_neighbour[2]: index_smallest_neighbour=(i,j)
-    # if nether of nodes exist create new set for the,
-    if index_smallest_neighbour==(-1,-1) and index_current_node==(-1,-1):
-        connected_map.append({smallest_neighbour,current_node})
-    elif index_smallest_neighbour==(-1,-1) and index_current_node!=(-1,-1):
-        connected_map[index_current_node[0]].add(smallest_neighbour)
-    elif index_smallest_neighbour!=(-1,-1) and index_current_node==(-1,-1):
-        connected_map[index_smallest_neighbour[0]].add(current_node)
-    else:
-        logging.info(index_smallest_neighbour)
-        connected_map[index_current_node[0]]=connected_map[index_current_node[0]].update(connected_map[index_smallest_neighbour[0]])
-        connected_map.pop(index_smallest_neighbour[0])
-    return connected_map
-
-def main(filename):
-    fd = open(filename, 'r')      
-    testCount = int(fd.readline())
-    for i in range(testCount):
-        (m, n) = [int(item) for item in fd.readline().split()]
-        alt_map = []
-        for i in range(m):
-            alt_map.append([{'data':int(k),'nxt':(-1,-1)} for k in fd.readline().split()])
-        #logging.info(alt_map)
-        for i in xrange(0,3):
-            for j in xrange(0,3):
-                getSmallestNeighbour(alt_map, i, j)
-        logging.info(alt_map)
+        if ar[0][3] < ar[1][3]: 
+            arry[x][y].set_next_node(arry[ar[0][1]][ar[0][2]])
+            logging.info('%d %d --> %d %d'%(x,y,ar[0][1],ar[0][2]))    
+        else : 
+            arry[x][y].set_next_node(arry[ar[1][1]][ar[1][2]]) 
+            logging.info('%d %d --> %d %d'%(x,y,ar[1][1],ar[1][2]))
+            
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
@@ -81,5 +87,16 @@ if __name__ == '__main__':
         st = sys.argv[1]
     else : st = 'smpl.in'
     tm = datetime.datetime.now()
-    main(st)
+    fd = open(st, 'r')      
+    testCount = int(fd.readline())
+    for i in range(testCount):
+        (m, n) = [int(item) for item in fd.readline().split()]
+        alt_map = []
+        for i in range(m):
+            alt_map.append([Node(int(k)) for k in fd.readline().split()])
+        #logging.info(alt_map)
+        [getSmallestNeighbour(alt_map, i, j) for j in range(n) for i in range(m)]
+        [j.getLabel() for i in alt_map for j in i ]
+        logging.info(alt_map)
+        print ('\n'.join(alt_map))
     logging.info(datetime.datetime.now() - tm)
