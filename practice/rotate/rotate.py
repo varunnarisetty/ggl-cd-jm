ed6a40b9efc5f1bf4391d4be64a7dea9a2cb671d
Santosh Dhanyamraju <geeniuss01@gmail.com>
2013-02-20 17:41:22 +0530

    Rotate - adding some code

--- practice/rotate/rotate.py
+++ practice/rotate/rotate.py
@@ -5,30 +5,39 @@ Created on Feb 20, 2013
 '''
 from pprint import pprint
 
-def main():
-    fd=open('smpl.in')
-    (n,n_cases)=[int(i) for i in fd.readline().strip().split()]
+
+def getConsecutiveCount(ar, element, x, y, inc_x, inc_y):
+    '''
+    returns sum of consecutive numbers
+    '''
+    n = len(ar)
+    if 0 <= x < n and 0 <= y < n and ar[x][y] == element:
+        return (getConsecutiveCount(ar, element, x - inc_x, y - inc_y, inc_x, inc_y) + 1 + getConsecutiveCount(ar, element, x + inc_x, y + inc_y, inc_x, inc_y))
+        
+    else: return 0
     
-    ar=list()
+
+def main():
+    fd = open('smpl.in')
+    (n, n_cases) = [int(i) for i in fd.readline().strip().split()]
+    
+    ar = list()
     for i in range(n):
-        k=[]
+        k = []
         for j in range(n):
             k.append('.')
         ar.append(k) 
-    pprint(ar)
-    ar[1][1]='b'
-    pprint(ar)
     for i in range(n):
-        y=[i1 for i1 in fd.readline().strip() if i1!='.']
+        y = [i1 for i1 in fd.readline().strip() if i1 != '.']
         pprint(y)
-        for j,el in enumerate(y):
-            print('%d,%d - %s'%(n-j-1,n-i-1,el))
-            ar[n-j-1][n-i-1]=el 
-            pprint(ar)
+        for j, el in enumerate(y):
+            print('%d,%d - %s' % (n - j - 1, n - i - 1, el))
+            ar[n - j - 1][n - i - 1] = el 
     pprint(ar)
+    pprint(getConsecutiveCount(ar, ar[6][2], 6, 2, 0, 1))
 
 if __name__ == '__main__':
-    x='..abc..'
-    y= [i for i in x if i!='.']
-    #pprint( y)
+    x = '..abc..'
+    y = [i for i in x if i != '.']
+    # pprint( y)
     main()
